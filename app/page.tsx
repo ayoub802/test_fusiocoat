c3c5a46aefbbf678af2a54e402f92aaf7b985c51
Programer Jr <yayoub2020@gmail.com>
2023-12-18 22:17:26 +0100

    Added

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-[#000] h-screen justify-center items-center flex'>
+      <h3 className='text-[#fff]'>Hello</h3>
+    </div>
+  )
+}
+
+export default page
